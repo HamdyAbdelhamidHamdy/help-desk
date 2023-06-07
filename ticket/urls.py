05227017a9cae8b3ffd2574c84eaacb2271b712c
HamdyAbdelhamidHamdy <Hamdy.abdelhameed@hugatech.com>
2023-06-07 18:30:24 +0300

    urls

--- ticket/urls.py
+++ ticket/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = 'ticket'
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('create-ticket/', create_ticket,  name='create-ticket'),
+    path('home/', home, name='home'),
+    path('', create_ticket,  name='create-ticket'),
 
 ]
